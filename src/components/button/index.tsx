397efce40940f1bef37c8c6151c0f46c22ac2e5f
Lucas Seccatto <lucasseccatto1@gmail.com>
2024-12-02 14:12:59 -0300

    Elitetracker Front

---
+++ src/components/button/index.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, forwardRef } from "react";
+import styles from "./styles.module.css";
+import clsx from "clsx";
+
+type ButtonProps = ComponentProps<"button"> & {
+  variant?: "info" | "error";
+};
+
+function Button({
+  children,
+  disabled,
+  variant = "info",
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={clsx(
+        styles.container,
+        variant === "error" && styles.error,
+        disabled && styles.disabled
+      )}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,31 @@
+import { GithubLogo } from "@phosphor-icons/react";
+import styles from "./styles.module.css";
+import Button from "../../components/button";
+
+import { api } from "../../services/api";
+
+function Login() {
+  async function handleAuth() {
+    const { data } = await api.get("/auth");
+
+    window.location.href = data.redirectUrl;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>Entre com</h1>
+        <Button onClick={handleAuth}>
+          <GithubLogo />
+          GitHub
+        </Button>
+        <p>
+          Ao entrar, eu concordo com os termos de serviço e a politica de
+          privacidade.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
